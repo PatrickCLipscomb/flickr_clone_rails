42b16064c039736bdf2dde900bb3c757ac1d411a
Brian Grant and patrick lipscomb <brian+patrick@>
2016-09-14 09:18:54 -0700

    add devise, create user and images models

---
+++ flickr_clone/app/controllers/images_controller.rb
@@ -0,0 +1,42 @@
+class ImagesController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
+  def index
+    @images = Image.all
+  end
+  def show
+    @image = Image.find(params[:id])
+  end
+  def edit
+    @image = Image.find(params[:id])
+  end
+  def new
+    @image = Image.new
+  end
+  def create
+    @image = Image.create(image_params)
+    if @image
+      flash[:notice] = "Image saved successfully"
+      redirect_to images_path
+    else
+      flash[:alert] = "Image failed to save"
+      render :new
+    end
+  end
+  def update
+    @image = Image.find(params[:id])
+    @image = @image.update(image_params)
+    redirect_to images_path
+  end
+  def destroy
+    if @image.delete
+      flash[:notice] = "Image deleted"
+      redirect_to images_path
+    else
+      flash[:alert] = "Image failed to delete"
+    end
+  end
+  private
+  def image_params
+    params.require(:image).permit(:email, :image)
+  end
+end
---
+++ flickr_clone/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root :to => 'images#index'
+  devise_for :users
+  resources :images
+end
